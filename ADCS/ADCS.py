4ddc586ab51a8ee6f63179263a331bcd72593569
Ahristi <thomasvmholland@outlook.com>
2023-10-07 01:21:39 +1100

    Cleaned up ADCS code

--- ADCS/ADCS.py
+++ ADCS/ADCS.py
@@ -30,88 +30,6 @@ class ADCS:
         """
 
         #Initial guess
-        guess = np.array([-2.0,2.0,2.0])
-        delta_x = np.array([1,1,1])
-    
-        max_iter = 3000
-        tol = 1e-16
-        iter_count = 0
-
-        #For now just assume only sensors are magnetometer and startracker
-        allSensors = [self.starTracker, self.magnetometer]
-
-        #3 times length for 3 outputs and 3 rows for 3 inputs
-        H = np.zeros((3 * len(allSensors), 3))
-
-        delta_y = np.zeros((3 *  len(allSensors), 1))
-        starName = "kentauras"
-        actualAttitude = self.satellite.attitude
-
-        #Get unit vector for the sensor readings
-        startrackReading = self.starTracker.getReading(starName, self.satellite.X, actualAttitude)
-        startrackReading = startrackReading/np.linalg.norm(startrackReading)
-
-        magnetReading = self.magnetometer.getReading(self.satellite.time, self.satellite.X, actualAttitude)
-        magnetReading = magnetReading/np.linalg.norm(magnetReading)
-
-        #Get unit vectors of the actual readings
-        starActual = self.starTracker.getActualReading(starName, self.satellite.X)
-        starActual = starActual/np.linalg.norm(starActual)
-
-        magnetActual = self.magnetometer.getActualReading(self.satellite.time, self.satellite.X)
-        magnetActual = magnetActual/np.linalg.norm(magnetActual)
-
-
-
-        while np.linalg.norm(delta_x) > tol:
-            
-            # Build H matrix
-            psi   = guess[0]
-            theta = guess[1]
-            phi  = guess[2]
-            
-
-            H_starTracker   =   jacobianDCM(psi,theta,phi, startrackReading)
-            H_magnet        =   jacobianDCM(psi,theta,phi, magnetReading)
-      
-            H[0:3, :] = H_starTracker
-            H[3:6, :] = H_magnet
-          
-            C = np.array([[np.cos(theta)*np.cos(psi), np.cos(theta)*np.sin(psi), -np.sin(theta)],
-                          [np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.sin(phi)*np.sin(theta)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.sin(phi)*np.cos(theta)],
-                          [np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi), np.cos(phi)*np.sin(theta)*np.sin(psi)- np.sin(phi)*np.cos(psi), np.cos(phi)*np.cos(theta)]])
-            
-            #Calculate residuals   
-
-            #print(self.magnetometer.getActualReading(self.satellite.time, self.satellite.X))
-            #print(C.T @ magnetReading)
-  
-            delta_y[0:3, 0] = (starActual - C.T @ startrackReading)
-            delta_y[3:6, 0] = (magnetActual - C.T @ magnetReading)
-            
-            # Use non-linear least squares to estimate error in x
-            delta_x = np.linalg.inv(H.T @ H) @ H.T @ delta_y
-
-            guess += delta_x.flatten()
-            
-            iter_count += 1
-
-            if iter_count >= max_iter:
-                print('Failed to Converge !!')
-                break
-  
-        return guess
-
-
-    def determineAttitudeINV(self):
-        """
-            determineAttitude:
-
-            Determines the satellite attitude based on the sensor readings
-            by applying weighted non linear least squares.        
-        """
-
-        #Initial guess
         guess = np.array([3.0,3.0,-1.0])
         delta_x = np.array([1,1,1])
     
@@ -154,8 +72,8 @@ class ADCS:
             theta = guess[1]
             psi   = guess[2]
 
-            H_starTracker   =   jacobianDCMINV(psi,theta,phi, starActual)
-            H_magnet        =   jacobianDCMINV(psi,theta,phi, magnetActual)
+            H_starTracker   =   jacobianDCM(psi,theta,phi, starActual)
+            H_magnet        =   jacobianDCM(psi,theta,phi, magnetActual)
       
             H[0:3, :] = H_starTracker
             H[3:6, :] = H_magnet
@@ -169,9 +87,8 @@ class ADCS:
             #print(self.magnetometer.getActualReading(self.satellite.time, self.satellite.X))
             #print(C.T @ magnetReading)
           
-            delta_y[0:3, 0] = (startrackReading - C @ starActual)
-            delta_y[3:6, 0] = (magnetReading    - C @ magnetActual)
-
+            delta_y[0:3, 0] =   (startrackReading - C @ starActual)
+            delta_y[3:6, 0] =   (magnetReading    - C @ magnetActual)
 
             # Use non-linear least squares to estimate error in x
             delta_x = np.linalg.inv(H.T @ H) @ H.T @ delta_y
@@ -187,50 +104,8 @@ class ADCS:
         return guess
 
 
+
 def jacobianDCM(yaw, pitch, roll, obs):
-    """ 
-        jacobianDCM
-
-        Computes the jacobian of the directional cosine matrix
-
-        Inputs;
-
-        yaw:    yaw (psi) euler angle of the satellite in ECI frame
-        pitch:  pitch (theta) euler angle of the satellite in ECI frame 
-        roll:   roll (phi) euler angle of the satellite in ECI frame
-        obs:    observation made of a known vector in the body frame
-
-    """
-    #Convenience variables
-    xb      =  obs[0]
-    yb      =  obs[1]
-    zb      =  obs[2]
-    psi     =  yaw
-    theta   =  pitch
-    phi     =  roll  
-
-    
-    dxdpsi      =   -xb * np.cos(theta) * np.sin(psi) + yb * (-np.sin(phi) * np.sin(theta) * np. sin(psi) - np.cos(phi) * np.cos(psi)) + zb * (-np.cos(phi) * np.sin(theta) * np.sin(psi) + np.sin(theta) * np.cos(psi))
-    dxdtheta    =   -xb*np.sin(theta) * np.cos(psi)   + yb * np.sin(phi) * np.cos(theta) * np.cos(psi)+ zb * np.cos(phi) * np.cos(theta) * np.cos(psi)
-    dxdphi      =    yb*(np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi)) + zb* (-np.sin(phi) * np.sin(theta)*np.cos(psi) + np.cos(phi) * np.sin(psi))
-    
-    dydpsi      =   xb*np.cos(theta) * np.cos(psi) + yb*(np.sin(phi) * np.sin(theta) * np.cos(psi) - np.cos(phi) * np.sin(psi)) + zb * (np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi))
-    dydtheta    =   -xb * np.sin(theta) * np.sin(psi) + yb * np.sin(phi) * np.cos(theta) * np.sin(psi) + zb* np.cos(phi) * np.cos(theta) * np.sin(psi)
-    dydphi      =   yb*(np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi)) + zb* (-np.sin(phi) * np.sin(theta) * np.sin(psi) - np.cos(phi) * np.cos(psi))
-
-    dzdpsi      =   0
-    dzdtheta    =   -xb*np.cos(theta) - yb* np.sin(phi) * np.sin(theta) - zb * np.cos(phi) * np.sin(theta)
-    dzdphi      =   yb * np.cos(phi) * np.cos(theta) - zb * np.sin(phi) * np.cos(theta)
-
-
-    H = np.array([[dxdpsi, dxdtheta, dxdphi],
-                 [dydpsi, dydtheta, dydphi],
-                 [dzdpsi, dzdtheta, dzdphi]])
-    
-    return H
-
-
-def jacobianDCMINV(yaw, pitch, roll, obs):
     """ 
         jacobianDCM
 
@@ -288,7 +163,7 @@ if __name__ == "__main__":
     sat.setAttitude(np.array([roll,pitch,yaw]))
     sat.ADCS.connectToSatellite(sat)
     print(np.array([roll,pitch,yaw]))
-    print(sat.ADCS.determineAttitudeINV())
+    print(sat.ADCS.determineAttitude())
 
     
 

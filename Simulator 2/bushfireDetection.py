334506c124fd826a8a490da27b4d6fb90593187c
Ahristi <thomasvmholland@outlook.com>
2023-10-29 19:48:37 +1100

    Close to fully integrated

--- Simulator 2/bushfireDetection.py
+++ Simulator 2/bushfireDetection.py
@@ -8,40 +8,29 @@ import numpy as np
 from PIL import Image
 
 # Open the TIFF file
-tiff_file = "landsat.tif"
+tiff_file = "eeResults/piliga_historic.tif"
 image = Image.open(tiff_file)
 
-# You can now manipulate the image or extract information from it.
-# For example, you can display the image:
-image.show()
 
-# To access pixel data, you can convert the image to a NumPy array
-import numpy as np
-image_data = np.array(image)
-
-# To access image information, such as dimensions and mode (e.g., "RGB" or "L" for grayscale), you can use the following attributes:
-width, height = image.size
-image_mode = image.mode
-
-print(f"Image dimensions: {width} x {height}")
-print(f"Image mode: {image_mode}")
-
-
-
-
-
-
-
-
-# Load the image
-image = cv2.imread('Results/onelayer1.jpg')
+image = np.array(image)
+cv2.imwrite("eeResults/piliga_historic.jpg", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
 # Apply filter
-lower1 = np.array([0, 0, 66])
-upper1 = np.array([5, 5, 97])
+lower1 = np.array([50, 0, 0])
+upper1 = np.array([90, 2, 2])
 mask = cv2.inRange(image, lower1, upper1)
 
- 
+white = 0
+total = 0
+for row in mask:
+    for pixel in row:
+        if (pixel == 255):
+            white+=1
+        total+=1
+
+ratio =white/total * 100
+print(ratio)
+
 # Display the image with detected blobs
 cv2.imshow('Image with Blobs', mask)
 cv2.waitKey(0)
